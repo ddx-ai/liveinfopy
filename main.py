f1b4a49053d661dcd4676d592fbc75b9673c0010
aoirint <aoirint@gmail.com>
2022-11-28 00:02:41 +0900

    call cli from main.py

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
 import liveinfo
 
-
-def main():
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument('live_id_or_url', type=str)
-  parser.add_argument('-s', '--service', type=str, choices=['nicolive'])
-  args = parser.parse_args()
-
-  live_id_or_url = args.live_id_or_url
-  service = args.service
-
-  print(
-    liveinfo.get_live_program(
-      live_id_or_url=live_id_or_url,
-      service=service,
-    )
-  )
-
-
 if __name__ == '__main__':
-  main()
+  liveinfo.cli.cli()
