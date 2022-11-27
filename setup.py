dd9d389907483efb719a0a830052b4856eef9379
aoirint <aoirint@gmail.com>
2022-11-27 21:57:15 +0900

    fix flake8

--- setup.py
+++ setup.py
@@ -5,39 +5,45 @@ from live_info_api_client import __VERSION__ as VERSION
 
 
 setup(
-    name='live_info_api_client',
-    version=VERSION, # '0.1.0-alpha', # == 0.1.0-alpha0 == 0.1.0a0
-    license='MIT',
+  name='live_info_api_client',
 
-    packages=find_packages(),
-    include_package_data=True,
+  # version example
+  #   '0.1.0-alpha', # == 0.1.0-alpha0 == 0.1.0a0
+  version=VERSION,
 
-    # entry_points = {
-    #     'console_scripts': [
-    #         # create `main` function in PACKAGE_NAME/scripts/my_command_module.py
-    #         'my_command_name = PACKAGE_NAME.scripts.my_command_module:main',
-    #     ],
-    # },
+  license='MIT',
 
-    install_requires=Path('requirements.in').read_text(encoding='utf-8').splitlines(),
+  packages=find_packages(),
+  include_package_data=True,
 
-    author='aoirint',
-    author_email='aoirint@gmail.com',
+  # entry_points = {
+  #   'console_scripts': [
+  #     # create `main` function in
+  #     #   live_info_api_client/hoge.py
+  #     'hoge = live_info_api_client.hoge:main',
+  #   ],
+  # },
 
-    url='https://github.com/aoirint/live_info_api_client_py',
-    # description='SHORT_DESCRIPTION',
+  install_requires=Path('requirements.in')
+    .read_text(encoding='utf-8').splitlines(),
 
-    long_description=open('README.md', 'r').read(),
-    long_description_content_type='text/markdown',
+  author='aoirint',
+  author_email='aoirint@gmail.com',
 
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
+  url='https://github.com/aoirint/live_info_api_client_py',
+  # description='SHORT_DESCRIPTION',
+
+  long_description=open('README.md', 'r').read(),
+  long_description_content_type='text/markdown',
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
 )
