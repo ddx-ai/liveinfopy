3cfc2125d9e43a683c6f26335ae6938bb45837d9
ddx-ai <magdk2536@gmail.com>
2025-05-03 17:58:21 +0900

    Update nicolive.py

--- liveinfo/nicolive.py
+++ liveinfo/nicolive.py
@@ -411,11 +411,5 @@ def parse_json_ld_in_nicolive_watch_html(
   return ParseJsonLdInNicoliveWatchHtmlSuccessJsonLdResult(
     result_type='success',
     data_type='json_ld',
-    data='[{
-      "name" : name,
-      "description" : description,
-      "thumbnail_url" : thumbnail_url,
-      "start_date" : start_date,
-      "end_date" : end_date
-    }]'
+    data='[{"name":'+name+',"description":'+description+',"thumbnail_url":'+ thumbnail_url+''","start_date" :'+start_date+',"end_date":'+end_date+'}]'
   )
